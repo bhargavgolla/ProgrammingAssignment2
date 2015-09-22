ff844e55c8aef9b314322dbb5e52aa26f44ade64
Golla, Bhargav(bgolla) <mail@bhargavgolla.com>
2015-09-22 08:29:01 -0700

    Solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions create a cacheable matrix inverse
 
-## Write a short comment describing this function
+## makeCacheMatrix takes a matrix as input and returns
+## the special cacheable matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+    # Initialize a null inverse matrix
+    inv <- NULL
+
+    # set matrix into local data
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+
+    # return actual matrix
+    get <- function() x
+
+    # set inverse into cache
+    setInv <- function(new_inv) inv <<- new_inv
+
+    # get inverse from cache
+    getInv <- function() inv
+
+    list(set = set, get = get,
+     setInv = setInv,
+     getInv = getInv)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes inverse of the special matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # get the cached version and check if it is valid
+    inv <- x$getInv()
+
+    if (!is.null(inv)) {
+        message("getting cached inverse")
+        return(inv)
+    }
+    
+    # cached inverse is null
+    # we calculate and update cache
+    data <- x$get()
+    inv <- solve(data)
+    x$setInv(inv)
+    inv
 }
